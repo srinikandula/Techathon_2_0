4dbf61a8445c6e914cf158726b35704c4ff6303f
atulmahindra <jadhav.atul3mahindralogistics.com>
2025-04-28 12:14:42 +0530

    ui kit updated

---
+++ src/_mock/_mock.ts
@@ -0,0 +1,77 @@
+export const _id = (index: number) => `e99f09a7-dd88-49d5-b1c8-1daf80c2d7b${index}`;
+
+export const _times = (index: number) =>
+  // 'MM/DD/YYYY'
+  [
+    '11/08/2023',
+    '04/09/2024',
+    '09/12/2023',
+    '01/01/2024',
+   
+  ][index];
+
+export const _fullName = (index: number) =>
+  [
+    'Billy Stoltenberg',
+    'Eloise Ebert',
+    'Teresa Luettgen',
+    'Salvador Mayert',
+  
+  ][index];
+
+export const _price = (index: number) =>
+  [
+    35.17, 57.22, 64.78, 50.79
+  ][index];
+
+export const _company = (index: number) =>
+  [
+    'Medhurst, Moore and Franey',
+    'Hahn, Homenick and Lind',
+    'Larkin LLC',
+    'Stamm, Larson and Mertz',
+    
+  ][index];
+
+export const _boolean = (index: number) =>
+  [
+    true,
+    false,
+    true,
+    false,
+   
+  ][index];
+
+export const _postTitles = (index: number) =>
+  [
+    'Whiteboard Templates By Industry Leaders',
+    'Tesla Cybertruck-inspired camper trailer for Tesla fans who can’t just wait for the truck!',
+    'Designify Agency Landing Page Design',
+    '✨What is Done is Done ✨',
+  
+  ][index];
+
+export const _description = (index: number) =>
+  [
+    'The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J',
+    'New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart',
+    'Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals',
+    'The Football Is Good For Training And Recreational Purposes',
+   
+  ][index];
+
+export const _taskNames = (index: number) =>
+  [
+    `Prepare Monthly Financial Report`,
+    `Design New Marketing Campaign`,
+    `Analyze Customer Feedback`,
+    `Update Website Content`,
+  ][index];
+
+export const _productNames = (index: number) =>
+  [
+    'Nike Air Force 1 NDESTRUKT',
+    'Nike Space Hippie 04',
+    'Nike Air Zoom Pegasus 37 A.I.R. Chaz Bear',
+    'Nike Blazer Low 77 Vintage',
+  ][index];
